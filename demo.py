9f3c8a04cc195f1c16d36feac9cce2b45b7335b8
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2024-12-15 11:13:31 +0530

    1st pyhton

---
+++ demo.py
@@ -0,0 +1,12 @@
+class solution:
+    def name(self):
+        print("Hi, my name is Hemanth")
+    def name1(self):
+        a=str(input("Enter your second name "))
+        b=str(input("Enter the clg name "))
+        print(f"The name is {a} and the clg name is {b}")
+
+if __name__ == "__main__":
+    obj = solution()
+    obj.name()
+    obj.name1()
