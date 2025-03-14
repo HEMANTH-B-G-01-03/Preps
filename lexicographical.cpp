f3236c03b6a875d6ca948f1bc2a661a7ff26fefa
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2025-03-14 17:08:40 +0530

    lexicographical substring

---
+++ lexicographical.cpp
@@ -0,0 +1,4 @@
+//given 2strings of lowercase select exactly  one non-empty substring  of s  and replce each charecter of it  with the previous charecter of the english alphabet For 
+// find the lecological smallest sring that can be obtained  after performing   the above operation exactly once 
+// lexicograpghical oreder means like a, b , c , d , e , f , g , h , i , j , k , l , m ... 
+
