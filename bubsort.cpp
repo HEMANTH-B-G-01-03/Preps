1d1d9df28aef5350c032128081f17c70195da92e
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2025-05-16 19:03:58 +0530

    selection sort

--- bubsort.cpp
+++ bubsort.cpp
@@ -1,4 +1,38 @@
 
+// #include <iostream>
+// #include <string>
+// #include<vector>
+// using namespace std;
+// #include<algorithm>
+
+// void bubsort(int a[], int n ){
+// for(int i =0;i<n-1;i++){
+//     for(int j=0;j<n-i-1;j++){
+//         if(a[j]>a[j+1]){
+//             swap(a[j],a[j+1]);
+//         }
+//     }
+// }
+
+// }
+// void printarr(int a[], int n) {
+//     for( int i=0;i<n;i++){
+//         cout<<a[i];
+//     }
+// }
+//  int main (){
+//  int  n =5;
+//  int a[]= { 4 , 2 , 1 , 6 , 3 , 9};
+
+//  bubsort(a , n);
+//  printarr(a , n);
+//     return 0;
+//  }
+
+
+// for optimisation 
+
+
 #include <iostream>
 #include <string>
 #include<vector>
@@ -7,10 +41,15 @@ using namespace std;
 
 void bubsort(int a[], int n ){
 for(int i =0;i<n-1;i++){
+    bool is_swap = false;
     for(int j=0;j<n-i-1;j++){
         if(a[j]>a[j+1]){
             swap(a[j],a[j+1]);
+            is_swap = true;
         }
+    }
+    if( !is_swap){ // array is already sorted 
+        return;
     }
 }
 
@@ -28,6 +67,4 @@ void printarr(int a[], int n) {
  printarr(a , n);
     return 0;
  }
-
-
 
---
+++ selectionsort.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <string>
+#include<vector>
+using namespace std;
+#include<algorithm>
+
+void bubsort(int a[], int n ){
+for(int i =0;i<n-1;i++){
+  int small_idx=i; // unsoted part starting  
+  for(int j=i+1;j<n;j++) {
+    if(a[j]<a[small_idx]){
+        small_idx = j;
+    }
+   
+  }
+ swap(a[i],a[small_idx]);
+    }
+}
+
+void printarr(int a[], int n) {
+    for( int i=0;i<n;i++){
+        cout<<a[i];
+    }
+}
+ int main (){
+
+ int  n =5;
+ int a[]= { 4 , 2 , 1 , 6 , 3 , 9};
+
+ bubsort(a , n);
+ printarr(a , n);
+    return 0;
+ }
