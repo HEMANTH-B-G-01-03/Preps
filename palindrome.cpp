636cbb1b9162884cefa67c0b3232441704b59ec0
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2025-05-29 16:27:55 +0530

    palin in nums

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,20 +1,58 @@
+// #include<iostream>
+// #include<string.h>
+// #include<strings.h>
+// #include<algorithm>
+// using namespace std;
+// int main(){
+//     string a ;
+//     cin>>a;
+//     string news=a;
+
+//     reverse(news.begin(),news.end());
+//     if(a==news){
+//         cout<<"it is a palindrome";
+//     }
+//     else{
+//          cout<<" it is not a palindrome ";
+//     }
+
+//     return 0;
+// }
+
+
 #include<iostream>
 #include<string.h>
 #include<strings.h>
 #include<algorithm>
 using namespace std;
-int main(){
-    string a ;
-    cin>>a;
-    string news=a;
 
-    reverse(news.begin(),news.end());
-    if(a==news){
-        cout<<"it is a palindrome";
+
+bool ispalind(int n ) {
+    int num = n;
+    int rev =0 ;
+        while(num!=0) {
+            int rem = n % 10;
+            rev = rev*10+rem;
+            num /= 10;
+        }
+        if(rev== num){
+            return true;
+        }
+        else {
+            return false;
+        }
+}
+int main(){
+    int  n;
+    cin>>n;
+    
+    if(ispalind(n))
+    {
+        cout<<"yes it is ";
     }
     else{
-         cout<<" it is not a palindrome ";
-    }
+        cout<<"No it isnt";
 
+    }
     return 0;
 }
